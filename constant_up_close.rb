20e1fbd114bad160a06ef788c074d141f5d632fd
Antonio Chinea <creative.studio.acts@gmail.com>
2011-10-05 19:59:05 -0430

    Class Methods, Instances Methods and Constants Up Close

---
+++ constant_up_close.rb
@@ -0,0 +1,8 @@
+class Ticket
+  VENUES = ["Convention Center", "Fairgrounds", "Town Hall"]
+end
+
+puts "We've closed the class definition."
+puts "So we have to use the path notation to reach the constant."
+puts "The venues are:"
+puts Ticket::VENUES
