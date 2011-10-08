286d13ea3135dbbb1120cdbd72c4fc3651896403
Antonio Chinea <creative.studio.acts@gmail.com>
2011-10-07 20:08:02 -0430

    Finished Chp.4 -- Now we know how to nest elements inside each other

---
+++ ch4/two_modules.rb
@@ -0,0 +1,19 @@
+module M
+  def report
+    puts "'report' method in module M"
+  end
+end
+
+module N
+  def report
+    puts "'report' method in module N"
+  end
+end
+
+class C
+  include M
+  include N
+end
+
+c = C.new
+c.report
